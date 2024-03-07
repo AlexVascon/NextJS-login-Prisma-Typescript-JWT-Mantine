66b9aa62cb72b9d6c815eeb445254ae9ae305fa5
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2024-03-07 18:58:37 +0100

    feat: add prisma and postgres connection

---
+++ prisma/client.ts
@@ -0,0 +1,13 @@
+// prisma/client.ts
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
+export default prisma;
